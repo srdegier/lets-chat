1d9fe26e508426556b17103dfb0dbf40f8f8eeda
srdegier <stefan_de_gier@outlook.com>
2023-04-19 15:00:28 +0200

    Added basic structure project

--- lets-talk/DashboardViewController.swift
+++ lets-talk/DashboardViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DashboardController.swift
 //  lets-talk
 //
 //  Created by Stefan de Gier on 19/04/2023.
@@ -7,11 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DashboardViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("DashboardViewController")
     }
 
 
