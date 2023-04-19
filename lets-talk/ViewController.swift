19a34da226e97138a71529641eec76bf1bfc7dc2
srdegier <stefan_de_gier@outlook.com>
2023-04-19 14:35:08 +0200

    Initial Commit

---
+++ lets-talk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lets-talk
+//
+//  Created by Stefan de Gier on 19/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
