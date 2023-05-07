1a807a1c23efbe82780999bcb75c608cb8c52754
srdegier <stefan_de_gier@outlook.com>
2023-05-07 17:18:44 +0200

    Removed comment code

--- lets-talk/View/Chat/ChatViewController.swift
+++ lets-talk/View/Chat/ChatViewController.swift
@@ -10,25 +10,11 @@ import UIKit
 
 class ChatViewController: UIViewController {
     
+    @IBOutlet weak var avatarMessageView: AvatarMessageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Chat"
-//        let gearImage = UIImage(systemName: "gear")
-//
-//        let gearImageView = UIImageView(image: gearImage)
-//        gearImageView.contentMode = .scaleAspectFit
-//
-//        let titleLabel = UILabel()
-//        titleLabel.text = "Settings"
-//        titleLabel.font = UIFont.systemFont(ofSize: 12) // vervang door de gewenste lettergrootte
-//        titleLabel.textAlignment = .center
-//
-//        let titleStackView = UIStackView(arrangedSubviews: [gearImageView, titleLabel])
-//        titleStackView.axis = .vertical
-//        titleStackView.spacing = 5 // vervang door de gewenste afstand tussen de afbeelding en de tekst
-//        titleStackView.alignment = .center
-//
-//        navigationItem.titleView = titleStackView
         print("ChatViewController")
     }
+    
 }
