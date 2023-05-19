f7da7a55fd5f0bfe328077120dabeab616cee735
srdegier <stefan_de_gier@outlook.com>
2023-05-19 16:38:47 +0200

    Added collectionview with demo messages and chatinputview

---
+++ lets-talk/Components/ChatView/ChatCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  ChatCollectionViewCell.swift
+//  lets-talk
+//
+//  Created by Stefan de Gier on 18/05/2023.
+//
+
+import UIKit
+
+class ChatCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var messageBubbleView: MessageBubbleView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+
+    func configure(with chatMessage: String, isChatMode: Bool, messageType: MessageType) {
+        self.messageBubbleView.chatMessage = chatMessage
+        self.messageBubbleView.isChatMode = isChatMode
+        self.messageBubbleView.messageType = messageType
+    }
+    
+    override func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+        let attributes = super.preferredLayoutAttributesFitting(layoutAttributes)
+        
+        let messageBubbleHeight = self.messageBubbleView.textBubbleView.bounds.height
+        let desiredHeight = messageBubbleHeight
+        
+        attributes.size.height = desiredHeight + 15
+        
+        return attributes
+    }
+
+
+
+}
+
+
