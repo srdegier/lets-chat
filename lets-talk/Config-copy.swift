ebcb0be65181e1728d4952431d5bba5cf0a476ed
Stefan de Gier <stefan_de_gier@outlook.com>
2023-05-25 21:42:32 +0200

    Config for development secret api key

---
+++ lets-talk/Config-copy.swift
@@ -0,0 +1,14 @@
+//
+//  Config-copy.swift
+//  lets-talk
+//
+//  Created by Stefan de Gier on 25/05/2023.
+//
+
+import Foundation
+
+struct Config {
+    static var apiKey: String {
+        return ""
+    }
+}
