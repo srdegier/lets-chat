c2da690121b5d26ba381a7c0e111f6407366a428
Stefan de Gier <stefan_de_gier@outlook.com>
2023-05-28 18:23:13 +0200

    Added Config file for api key (note next commit will ignore any updates on the config file)

---
+++ lets-talk/Config.swift
@@ -0,0 +1,16 @@
+//
+//  Config.swift
+//  lets-talk
+//
+//  Created by Stefan de Gier on 28/05/2023.
+//
+
+//TODO: make own file named: "Config"
+
+import Foundation
+
+struct Config {
+    static var apiKey: String {
+        return ""
+    }
+}
