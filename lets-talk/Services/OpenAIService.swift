0a2acc26633699bec2ac0fb3475d784701026a14
Stefan de Gier <stefan_de_gier@outlook.com>
2023-05-28 17:37:20 +0200

    Added webservice with config env (git history change)

---
+++ lets-talk/Services/OpenAIService.swift
@@ -0,0 +1,31 @@
+//
+//  OpenAIService.swift
+//  lets-talk
+//
+//  Created by Stefan de Gier on 25/05/2023.
+//
+
+import Foundation
+import OpenAISwift
+
+class OpenAIService {
+    
+    // set apikey in the config file
+    let openAI = OpenAISwift(authToken: Config.apiKey)
+    
+    public func fetchRespond(payload:[ChatMessage]) async -> Result<OpenAI<MessageResult>, Error> {
+        do {
+            let chat: [ChatMessage] = payload
+                        
+            let result = try await openAI.sendChat(
+                with: chat,
+                model: .chat(.chatgpt),
+                maxTokens: 200
+            )
+            
+            return .success(result)
+        } catch  {
+            return .failure(error)
+        }
+    }
+}
