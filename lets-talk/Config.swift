ae5bc0e69e28f16e500baabcbfcf2732b669e070
Stefan de Gier <stefan_de_gier@outlook.com>
2023-05-28 19:13:31 +0200

    Added config file

---
+++ lets-talk/Config.swift
@@ -0,0 +1,14 @@
+//
+//  Config.swift
+//  lets-talk
+//
+//  Created by Stefan de Gier on 28/05/2023.
+//
+
+import Foundation
+
+struct Config {
+    static var apiKey: String {
+        return ""
+    }
+}
