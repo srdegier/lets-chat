0c248c2bbba5960b5df0aab1016c3a593365b7e6
Stefan de Gier <stefan_de_gier@outlook.com>
2023-06-04 18:22:32 +0200

    Added new pattern for adding navigation dashboard view

--- lets-talk/Views/ViewControllerFactory.swift
+++ lets-talk/Views/ViewControllerFactory.swift
@@ -8,19 +8,53 @@
 import Foundation
 import UIKit
 
+enum ViewControllerType {
+    case chat
+    case solutions
+    case settings
+    
+    var viewController: UIViewController? {
+        switch self {
+        case .chat:
+            return ViewControllerFactory.chatViewController()
+        case .solutions:
+            return ViewControllerFactory.solutionsViewController()
+        case .settings:
+            return ViewControllerFactory.settingsViewController()
+        }
+    }
+}
+
 class ViewControllerFactory {
     
     private static let chatStoryboard = UIStoryboard(name: "Chat", bundle: nil)
     private static let solutionsStoryboard = UIStoryboard(name: "Solution", bundle: nil)
+    private static let settingsStoryboard = UIStoryboard(name: "Settings", bundle: nil)
 
-    
-    internal static func chatViewController() -> UIViewController {
-        let vc = self.chatStoryboard.instantiateViewController(withIdentifier: "ChatViewController")
+    internal static func chatViewController() -> ChatViewController? {
+        let identifier = "ChatViewController"
+        guard let vc = self.chatStoryboard.instantiateViewController(withIdentifier: identifier) as? ChatViewController else {
+            assertionFailure("Storyboard ID '\(identifier)' not found or incorrect")
+            return nil
+        }
         return vc
     }
     
-    internal static func solutionsViewController() -> UIViewController {
-        let vc = self.solutionsStoryboard.instantiateViewController(withIdentifier: "SolutionViewController")
+    internal static func solutionsViewController() -> SolutionViewController? {
+        let identifier = "SolutionViewController"
+        guard let vc = self.solutionsStoryboard.instantiateViewController(withIdentifier: identifier) as? SolutionViewController else {
+            assertionFailure("Storyboard ID '\(identifier)' not found or incorrect")
+            return nil
+        }
+        return vc
+    }
+    
+    internal static func settingsViewController() -> SettingsViewController? {
+        let identifier = "SettingsViewController"
+        guard let vc = self.settingsStoryboard.instantiateViewController(withIdentifier: identifier) as? SettingsViewController else {
+            assertionFailure("Storyboard ID '\(identifier)' not found or incorrect")
+            return nil
+        }
         return vc
     }
 }
